1e1f2b459206441bdd887fe36f63d2621ba95a97
Aleksandr Vedeneev <a-vedeneev@ya.ru>
2022-11-24 18:10:04 +0300

    Modified Poster.java

--- src/main/java/my/idea/list/Poster.java
+++ src/main/java/my/idea/list/Poster.java
@@ -2,4 +2,25 @@ package my.idea.list;
 
 public class Poster {
 
+    private int id;
+    private String posterName;
+    private String posterGenre;
+
+    public Poster(int id, String posterName, String posterGenre) {
+        this.id = id;
+        this.posterName = posterName;
+        this.posterGenre = posterGenre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPosterName() {
+        return posterName;
+    }
+
+    public String getPosterGenre() {
+        return posterGenre;
+    }
 }
