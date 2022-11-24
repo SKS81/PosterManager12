af8a2fa8a8c6a62ce4efce7b3085bb4f3577053c
Aleksandr Vedeneev <a-vedeneev@ya.ru>
2022-11-24 18:12:48 +0300

    Modified PosterManager.java

--- src/main/java/my/idea/list/PosterManager.java
+++ src/main/java/my/idea/list/PosterManager.java
@@ -2,4 +2,50 @@ package my.idea.list;
 
 public class PosterManager {
 
+    private int countPoster = 500;
+    private Poster[] posters = new Poster[0];
+    public PosterManager(int countPoster) {
+        this.countPoster = countPoster;
+    }
+    public PosterManager() {};
+
+    public void addNewPoster(Poster poster) {
+        Poster[] temp = new Poster[posters.length + 1];
+        for (int i = 0; i < posters.length; i++) {
+            temp[i] = posters[i];
+        }
+        temp[temp.length - 1] = poster;
+        posters = temp;
+    }
+
+    public void removePosterById(int id) {
+        Poster[] temp = new Poster[posters.length - 1];
+        int copyToInd = 0;
+        for (Poster poster : posters) {
+            if (poster.getId() !=id) {
+                temp[copyToInd] = poster;
+                copyToInd++;
+            }
+        }
+        posters = temp;
+    }
+
+    public Poster[] getLastAll() {
+        int resLenght = posters.length;
+        if (resLenght < countPoster) {
+            resLenght = posters.length;
+        } else {
+            resLenght = countPoster;
+        }
+        Poster[] res = new Poster[resLenght];
+        for (int i = 0; i < resLenght; i++) {
+            int index = posters.length - i - 1;
+            res[i] = posters[index];
+        }
+        return res;
+    }
+
+    public Poster[] getAll() {
+        return posters;
+    }
 }
